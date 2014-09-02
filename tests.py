7c1248da9efea9a2c65c5c19e9ac8e5854327257
David Siedband <david@zentraal.com>
2014-09-02 05:08:44 -0300

    tests for the list view.  not passing yet, so this makes a good place to pick up next time

--- tests.py
+++ tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
+from django.test.client import Client
+from django.test.client import RequestFactory
 from .models import Aikidoka
-
+from .views import ListAikidokaView
 
 class AikidokaTests(TestCase):
     """Aikidoka model tests."""
@@ -11,3 +13,30 @@ class AikidokaTests(TestCase):
             str(aikidoka),
             'Morihiro Saito',
     )
+
+class AikidokaListViewTests(TestCase):
+    """Aikidoka list view tests."""
+
+    def test_aikidokas_in_the_context(self): 
+
+        client = Client()
+        response = client.get('/')
+
+        self.assertEquals(list(response.context['object_list']), [])
+
+        Aikidoka.objects.create(first_name='Gozo', last_name='Shioda')
+        response = client.get('/')
+        self.assertEquals(response.context['object_list'].count(), 1)
+
+    def test_aikidokas_in_the_context_request_factory(self):
+
+        factory = RequestFactory()
+        request = factory.get('/')
+
+        response = ListAikidokasView.as_view()(request)
+
+        self.assertEquals(list(response.context_data['object_list']), [])
+
+        Aikidoka.objects.create(first_name='Moriteru', last_name='Ueshiba')
+        response = ListAikidokasView.as_view()(request)
+        self.assertEquals(response.context_data['object_list'].count(), 1)
