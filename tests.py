39241853d650c180b88c4441abc8e394123a499d
David Siedband <david@zentraal.com>
2014-09-02 04:39:22 -0300

    Basic test before we create the Model

--- tests.py
+++ tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import Dojo
 
-# Create your tests here.
+
+class AikidokaTests(TestCase):
+    """Aikidoka model tests."""
+    
+    def test_str(self):
+        aikidoka = Aikidoka(first_name='Morihiro', last_name='Saito')
+        self.assertEquals(
+            str(aikidoka),
+            'Morihiro Saito',
+    )
