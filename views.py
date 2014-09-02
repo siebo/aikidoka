2563d3e92e19d487ae969500e0444d3c110bd4c8
David Siedband <david@zentraal.com>
2014-09-02 04:54:11 -0300

    list view for aikidoka

--- views.py
+++ views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import Aikidoka
+
+class ListAikidokaView(ListView):
+    model = Aikidoka
+    template_name = 'aikidoka_list.html'
